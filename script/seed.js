d63f9bd568dc25dd26e66ca26c0cd03d0b378294
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-16 00:26:02 -0500

    seeds some classes and some users (teachers & students)

--- script/seed.js
+++ script/seed.js
@@ -1,35 +1,92 @@
-'use strict'
+"use strict";
 
-const db = require('../server/db')
-const {User} = require('../server/db/models')
+const db = require("../server/db");
+const { User, CircusClass } = require("../server/db/models");
 
 async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
+  await db.sync({ force: true });
+  console.log("db synced!");
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+    User.create({ email: "murphy@email.com", password: "123" }),
+    User.create({ email: "cody@email.com", password: "123" }),
+    User.create({
+      email: "master-trainer@email.com",
+      password: "123",
+      userType: 1,
+      bio: "the very best like no one ever was",
+    }),
+    User.create({
+      email: "aerialista@email.com",
+      password: "123",
+      userType: 1,
+      bio: "will encourage you to brag about burns",
+      displayName: "Jenny Aerialst",
+    }),
+  ]);
+  console.log(`seeded ${users.length} users`);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  const coaches = [];
+  const students = [];
+  users.forEach(user => {
+    if (user.userType > 0) coaches.push(user);
+    else students.push(user);
+  });
+
+  console.log("c: ", coaches.length);
+  console.log("s: ", students.length);
+
+  const aerialista = coaches.find(
+    coach => coach.displayName === "Jenny Aerialst"
+  );
+  const masterTrainer = coaches.find(
+    coach => coach.displayName === "master-trainer"
+  );
+
+  const circusClasses = await Promise.all([
+    CircusClass.create({
+      title: "Advanced Beginning Silks",
+      description:
+        "Great for those who can both invert comfortably in the air and do not ask to do a double star every class.",
+      cap: 8,
+      instructorId: aerialista.id,
+    }),
+    CircusClass.create({
+      title: "Upper Back Flexibility",
+      description: "Because quarantine means we all have hunchbacks now.",
+      cap: 10,
+      instructorId: masterTrainer.id,
+    }),
+    CircusClass.create({
+      title: "Handstands",
+      description: "for a better profile pic",
+      cap: 7,
+      instructorId: masterTrainer.id,
+    }),
+  ]);
+  console.log(`seeded ${circusClasses.length} classes`);
+
+  const handstandsClass = circusClasses.find(cc => cc.title === "Handstands");
+
+  await handstandsClass.setUsers(students);
+
+  console.log(`seeded successfully`);
 }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
 // The `seed` function is concerned only with modifying the database.
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -37,8 +94,8 @@ async function runSeed() {
 // `Async` functions always return a promise, so we can use `catch` to handle
 // any errors that might occur inside of `seed`.
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
