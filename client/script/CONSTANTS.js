03ae166c085780ae1ee40f75b404fb6fdfc8a74f
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-18 11:59:01 -0500

    includes objects that may be used in several components

---
+++ client/script/CONSTANTS.js
@@ -0,0 +1,22 @@
+export const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const dayAndDateOptions = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+};
+
+export const hourAndMinuteOptions = {
+  hour: "numeric",
+  minute: "numeric",
+};
+
+export const timesOfDay = ["Morning", "Afternoon", "Evening"];
