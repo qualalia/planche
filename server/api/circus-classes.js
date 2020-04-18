799c681a79d4334b108795217a0c78432682ae8a
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-18 11:57:18 -0500

    sends relevent attributes for students & instructor in GET /classes and GET /classes/:id; adds POST route for new class

--- server/api/circus-classes.js
+++ server/api/circus-classes.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const { CircusClass, User } = require("../db/models");
+const { Op } = require("sequelize");
 module.exports = router;
 
 router.get("/", async (req, res, next) => {
   try {
     const allClasses = await CircusClass.findAll({
-      include: "instructor",
+      include: {
+        model: User,
+        as: "instructor",
+        attributes: ["id", "displayName", "userType", "bio"],
+      },
     });
     res.json(allClasses);
   } catch (err) {
@@ -15,11 +20,36 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const singleClass = await CircusClass.findByPk(req.params.id, {
-      include: "instructor",
-      include: [User],
-    });
-    res.json(singleClass);
+    if (typeof parseInt(req.params.id) !== "number") res.sendStatus(404);
+    else {
+      const singleClass = await CircusClass.findByPk(req.params.id, {
+        include: {
+          model: User,
+          as: "instructor",
+          attributes: ["id", "displayName", "userType", "bio"],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["id", "email", "displayName", "userType", "bio"],
+          },
+        ],
+      });
+      res.json({ singleClass });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    if (req.user.userType !== 1) {
+      res.sendStatus(401);
+    } else {
+      const newClass = await CircusClass.create(req.body);
+      res.status(201).json(newClass);
+    }
   } catch (err) {
     next(err);
   }
