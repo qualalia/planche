027cb227d24f81cf8c72c9768b705811cea9c199
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-19 09:29:28 -0500

    adjusts relationships for Lesson

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,12 +3,18 @@ const CircusClass = require("./circusClass");
 const LessonEvent = require("./lessonEvent.js");
 const Lesson = require("./lesson.js");
 
-//CircusClass.hasMany(User, { as: "student" });
-CircusClass.belongsToMany(User, { through: "StudentClass" });
-User.belongsToMany(CircusClass, { through: "StudentClass" });
+// Students n:m with Lesson
+Lesson.belongsToMany(User, { through: "StudentLesson" });
+User.belongsToMany(Lesson, { through: "StudentLesson" });
+
+// Instructor
 CircusClass.belongsTo(User, { as: "instructor" });
-Lesson.hasMany(LessonEvent);
+
+// Lessons
 CircusClass.hasMany(Lesson);
+Lesson.hasMany(LessonEvent);
+
+// Lesson Events
 LessonEvent.belongsTo(User);
 User.hasMany(LessonEvent);
 
