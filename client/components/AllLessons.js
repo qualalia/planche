66dca1fe1783d3688d9ff2f10ce5bd7d05fb33b2
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-19 19:31:31 -0500

    fetches classes based on date query

--- client/components/AllLessons.js
+++ client/components/AllLessons.js
@@ -4,17 +4,17 @@ import { fetchLessons } from "../store";
 import { SingleLesson } from "../components";
 import { todayDay, todayDate } from "../script";
 import { Grid, Label, Header } from "semantic-ui-react";
+import queryString from "query-string";
 
-const AllLessonsOnGivenDay = props => {
-  /* TODO : Refactor into All Lessons for the Given Day */
+const AllLessons = props => {
   const allLessons = useSelector(state => state.allLessons) || [];
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const { date } = props;
-
+  const parsed = queryString.parse(location.search);
   useEffect(
     () => {
-      dispatch(fetchLessons()); // todo: FOR THIS DAY
+      dispatch(fetchLessons(location.search));
       setLoading(false);
     },
     [allLessons.length]
@@ -24,19 +24,23 @@ const AllLessonsOnGivenDay = props => {
     <div className="day-container">
       {allLessons.error && <h2>Error: {allLessons.error}</h2>}
       {loading && <h2>Loading...</h2>}
-      {allLessons.length && (
+      {allLessons && (
         <div>
           <div className="day-of-the-week">
-            <Header as="h1" inverted className="day-of-the-week">
+            <Header inverted as="h1" className="day-of-the-week">
               {todayDay(date)}
               {", "}
               {todayDate(date)}
             </Header>
           </div>
           <Grid className="days-classes" columns="equal">
-            {allLessons.map(lesson => (
-              <SingleLesson key={`lesson${lesson.id}`} lesson={lesson} />
-            ))}
+            {allLessons.length > 0 ? (
+              allLessons.map(lesson => (
+                <SingleLesson key={`lesson${lesson.id}`} lesson={lesson} />
+              ))
+            ) : (
+              <Header as="h1" inverted content="No classes scheduled yet." />
+            )}
           </Grid>
         </div>
       )}
@@ -44,24 +48,4 @@ const AllLessonsOnGivenDay = props => {
   );
 };
 
-export default AllLessonsOnGivenDay;
-
-/*
-   <Grid.Row className="lesson-list-header">
-   <Grid.Column width={3}>
-   <Header as="h3" textAlign="left">
-   Time
-   </Header>
-   </Grid.Column>
-   <Grid.Column width={10}>
-   <Header as="h3" textAlign="left">
-   Class
-   </Header>
-   </Grid.Column>
-   <Grid.Column width={2}>
-   <Header as="h3" textAlign="left">
-   Instructor Studio
-   </Header>
-   </Grid.Column>
-   </Grid.Row>
- */
+export default AllLessons;
