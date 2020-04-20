9f463cc54e62fc8b59240300192dc34405b450a0
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-19 19:36:20 -0500

    clean up comments

--- client/store/user.js
+++ client/store/user.js
@@ -1,30 +1,16 @@
 import axios from "axios";
 import history from "../history";
 
-/**
- * ACTION TYPES
- */
 const GET_USER = "GET_USER";
 const REMOVE_USER = "REMOVE_USER";
 
-/**
- * INITIAL STATE
- */
-const defaultUser = {};
-
-/**
- * ACTION CREATORS
- */
 const getUser = user => ({ type: GET_USER, user });
 const removeUser = () => ({ type: REMOVE_USER });
 
-/**
- * THUNK CREATORS
- */
 export const me = () => async dispatch => {
   try {
     const res = await axios.get("/auth/me");
-    dispatch(getUser(res.data || defaultUser));
+    dispatch(getUser(res.data || {}));
   } catch (err) {
     console.error(err);
   }
@@ -58,12 +44,12 @@ export const logout = () => async dispatch => {
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+export default function(state = {}, action) {
   switch (action.type) {
     case GET_USER:
       return action.user;
     case REMOVE_USER:
-      return defaultUser;
+      return state;
     default:
       return state;
   }
