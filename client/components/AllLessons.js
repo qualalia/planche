ec00743cffa409305b064997071f604c1150bed9
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-20 14:27:39 -0500

    wip local storage

--- client/components/AllLessons.js
+++ client/components/AllLessons.js
@@ -6,44 +6,61 @@ import { todayDay, todayDate } from "../script";
 import { Grid, Label, Header } from "semantic-ui-react";
 import queryString from "query-string";
 
+console.log("local storage outside of all lessons: ", localStorage);
+
 const AllLessons = props => {
-  const allLessons = useSelector(state => state.allLessons) || [];
+  const lessons = useSelector(state => state.lessons.data);
+  const status = useSelector(state => state.lessons.status);
   const [loading, setLoading] = useState(true);
+  const [noLessons, setNoLessons] = useState(false);
   const dispatch = useDispatch();
   const { date } = props;
   const parsed = queryString.parse(location.search);
   useEffect(
     () => {
-      dispatch(fetchLessons(location.search));
-      setLoading(false);
+      if (localStorage.getItem(location.search)) {
+        console.log(
+          "already has that query: ",
+          localStorage.getItem(location.search)
+        );
+      } else {
+        (async () => {
+          await dispatch(fetchLessons(location.search));
+          console.log(lessons);
+          await localStorage.setItem(location.search, JSON.stringify(lessons));
+        })();
+        console.log("new query: ", localStorage.getItem(location.search));
+      }
+
+      if (status) setLoading(false);
+      if (status === 204) setNoLessons(true);
     },
-    [allLessons.length]
+    [lessons.length]
   );
 
   return (
     <div className="day-container">
-      {allLessons.error && <h2>Error: {allLessons.error}</h2>}
-      {loading && <h2>Loading...</h2>}
-      {allLessons && (
-        <div>
-          <div className="day-of-the-week">
-            <Header inverted as="h1" className="day-of-the-week">
-              {todayDay(date)}
-              {", "}
-              {todayDate(date)}
-            </Header>
-          </div>
+      <div>
+        <div className="day-of-the-week">
+          <Header inverted as="h1" className="day-of-the-week">
+            {todayDay(date)}
+            {", "}
+            {todayDate(date)}
+          </Header>
+        </div>
+        {lessons.error && <h2>Error: {lessons.error}</h2>}
+        {lessons && (
           <Grid className="days-classes" columns="equal">
-            {allLessons.length > 0 ? (
-              allLessons.map(lesson => (
+            {!noLessons ? (
+              lessons.map(lesson => (
                 <SingleLesson key={`lesson${lesson.id}`} lesson={lesson} />
               ))
             ) : (
               <Header as="h1" inverted content="No classes scheduled yet." />
             )}
           </Grid>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
