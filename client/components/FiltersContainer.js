48e6e123af55445264dc3a7eb1623bb78e2eb330
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-20 16:36:54 -0500

    makes query from chosen options

--- client/components/FiltersContainer.js
+++ client/components/FiltersContainer.js
@@ -3,12 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchInstructors, fetchSchools } from "../store";
 import { DropdownList } from "../components";
 import { daysOfTheWeek } from "../script";
-import { Label, Segment, Dropdown } from "semantic-ui-react";
+import { Label, Button, Dropdown } from "semantic-ui-react";
+import queryString from "query-string";
 
 const FiltersContainer = () => {
   const instructors = useSelector(state => state.instructors) || [];
   const schools = useSelector(state => state.schools.data) || [];
   const dispatch = useDispatch();
+  const [choices, setChoices] = useState({
+    days: [],
+    instructors: [],
+    schools: [],
+  });
+  const [chosenSchools, setChosenSchools] = useState([]);
+  const [chosenInstructors, setChosenInstructors] = useState([]);
+  const [chosenDays, setChosenDays] = useState([]);
+
   useEffect(
     () => {
       if (!instructors.length || !instructors.error)
@@ -17,11 +27,49 @@ const FiltersContainer = () => {
     },
     [instructors.length, schools.length]
   );
+
+  const handleChoose = (e, data) => {
+    const currentChoice = data.value;
+    console.log(data.name);
+    switch (data.name) {
+      case "Schools":
+        setChosenSchools(currentChoice);
+        return;
+      case "Instructors":
+        setChosenInstructors(currentChoice);
+        return;
+      case "Day":
+        setChosenDays(currentChoice);
+        return;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = () => {
+    const day = chosenDays.length ? chosenDays : null;
+    const instructor = chosenInstructors.length ? chosenInstructors : null;
+    const school = chosenSchools.length ? chosenSchools : null;
+    const query = queryString.stringify(
+      {
+        day,
+        instructor,
+        school,
+      },
+      { skipNull: true }
+    );
+    location.search = query;
+  };
+
   return (
     <div id="filters">
       <div className="filter-container">
         <Label className="filter-label" ribbon color="yellow" content="Day" />
-        <DropdownList list={daysOfTheWeek} listName={"Day"} />
+        <DropdownList
+          list={daysOfTheWeek}
+          listName={"Day"}
+          handleChoose={handleChoose}
+        />
       </div>
       <div className="filter-container">
         <Label
@@ -37,6 +85,7 @@ const FiltersContainer = () => {
               : []
           }
           listName="Instructors"
+          handleChoose={handleChoose}
         />
       </div>
       <div className="filter-container">
@@ -49,8 +98,10 @@ const FiltersContainer = () => {
         <DropdownList
           list={schools.length > 0 ? schools.map(school => school.name) : []}
           listName="Schools"
+          handleChoose={handleChoose}
         />
       </div>
+      <Button onClick={handleSubmit} content={"Submit"} color="purple" />
     </div>
   );
 };
