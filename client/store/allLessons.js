ae7cdbf1f0eb9b382b9c34d03cbb19ae7eb06eb5
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-20 14:30:00 -0500

    changes shape of data to include data, status, and error keys

--- client/store/allLessons.js
+++ client/store/allLessons.js
@@ -28,13 +28,29 @@ export function fetchLessons(query) {
   };
 }
 
+const initialState = { data: [], status: null, error: null };
+
 /**
  * REDUCER
  */
-export default function(state = {}, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case SET_LESSONS:
       return action.lessons;
+    /* if (action.lessons.status === 200) {
+       *   if (state.data.length <= 2)
+       *     return {
+       *       ...state,
+       *       data: [...state.data, action.lessons.data],
+       *     };
+       *   else {
+       *     return { ...state, data: action.lessons.data };
+       *   }
+       * } else if (action.lessons.status === 204) {
+       *   return { ...state, status: action.lessons.status };
+       * } else {
+       *   return action.lessons;
+       * } */
     case SET_LESSONS_ERROR:
       return { ...state, error: action.error.message };
     default:
