fc214d075baefa228c625b7bfde540a65bfe4483
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-21 13:19:22 -0500

    adjusts props destructuring to match query in GET /lessons

--- client/components/SingleLesson.js
+++ client/components/SingleLesson.js
@@ -6,8 +6,15 @@ import {
 } from "../script/CONSTANTS.js";
 
 const SingleLesson = props => {
-  const { circusClass, cap, startTime, endTime } = props.lesson;
-  const { description, title, company, instructor } = circusClass;
+  const {
+    circusClass,
+    cap,
+    startTime,
+    endTime,
+    instructor,
+    company,
+  } = props.lesson;
+  const { description, title } = circusClass;
   const instructorName = instructor.displayName;
   return (
     <Grid.Row columns={3} className="lesson-row">
